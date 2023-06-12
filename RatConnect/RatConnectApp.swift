e77f0032325aeeb0312aea4ab554f0ceec40a872
Shahadbagarish <Shahad.bagarish@gmail.com>
2023-06-12 10:41:11 +0300

    Initial Commit

---
+++ RatConnect/RatConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatConnectApp.swift
+//  RatConnect
+//
+//  Created by Shahad Bagarish on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RatConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
